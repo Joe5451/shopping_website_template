8a5c6d5953fe6685ff94e4181580284a5ae7bb58
Joe <skybluejoe123@gmail.com>
2022-05-02 23:05:46 +0800

    modify news list

---
+++ news_list.php
@@ -0,0 +1,192 @@
+<?php include('php/head.php'); ?>
+
+<div class="page_head_banner">
+    <img src="img/img2.png" alt="最新消息" class="page_head_banner_img">
+    <div class="page_head_banner_content">
+        <h1 class="page_head_banner_title">最新消息</h1>
+        <h2 class="page_head_banner_subtitle">News</h2>
+    </div>
+</div>
+
+<section class="mx-auto">
+    <div class="custom_container">
+        <nav class="page_breadcrumb">
+            <a href="welcome.php" class="page_breadcrumb_link">首頁</a>
+            <span class="page_breadcrumb_separator">〉</span>
+            <a href="news.php" class="page_breadcrumb_link active">最新消息</a>
+        </nav>
+    
+        <div class="news_category_nav custom_horizontal_scrollbar">
+            <a href="#" class="news_category_link">全部</a>
+            <a href="#" class="news_category_link">好康優惠</a>
+            <a href="#" class="news_category_link">體驗分享</a>
+        </div>
+    
+        <div class="news_list_container">
+            <div class="news_list">
+                <a href="#" class="news_list_link">
+                    <div class="news_list_img_wrap">
+                        <img src="img/img2.png" class="news_list_img">
+                        <div class="news_list_more_mask">MORE</div>
+                    </div>
+                    <div class="news_list_content">
+                        <div class="news_list_date">2022/05/01</div>
+                        <div class="news_list_title">最新消息標題最新消息標題</div>
+                        <div class="news_list_summary">最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要</div>
+                    </div>
+                </a>
+            </div>
+            <div class="news_list">
+                <a href="#" class="news_list_link">
+                    <div class="news_list_img_wrap">
+                        <img src="img/img2.png" class="news_list_img">
+                        <div class="news_list_more_mask">MORE</div>
+                    </div>
+                    <div class="news_list_content">
+                        <div class="news_list_date">2022/05/01</div>
+                        <div class="news_list_title">最新消息標題最新消息標題</div>
+                        <div class="news_list_summary">最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要</div>
+                    </div>
+                </a>
+            </div>
+            <div class="news_list">
+                <a href="#" class="news_list_link">
+                    <div class="news_list_img_wrap">
+                        <img src="img/img2.png" class="news_list_img">
+                        <div class="news_list_more_mask">MORE</div>
+                    </div>
+                    <div class="news_list_content">
+                        <div class="news_list_date">2022/05/01</div>
+                        <div class="news_list_title">最新消息標題最新消息標題</div>
+                        <div class="news_list_summary">最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要</div>
+                    </div>
+                </a>
+            </div>
+            <div class="news_list">
+                <a href="#" class="news_list_link">
+                    <div class="news_list_img_wrap">
+                        <img src="img/img2.png" class="news_list_img">
+                        <div class="news_list_more_mask">MORE</div>
+                    </div>
+                    <div class="news_list_content">
+                        <div class="news_list_date">2022/05/01</div>
+                        <div class="news_list_title">最新消息標題最新消息標題</div>
+                        <div class="news_list_summary">最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要</div>
+                    </div>
+                </a>
+            </div>
+            <div class="news_list">
+                <a href="#" class="news_list_link">
+                    <div class="news_list_img_wrap">
+                        <img src="img/img2.png" class="news_list_img">
+                        <div class="news_list_more_mask">MORE</div>
+                    </div>
+                    <div class="news_list_content">
+                        <div class="news_list_date">2022/05/01</div>
+                        <div class="news_list_title">最新消息標題最新消息標題</div>
+                        <div class="news_list_summary">最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要</div>
+                    </div>
+                </a>
+            </div>
+            <div class="news_list">
+                <a href="#" class="news_list_link">
+                    <div class="news_list_img_wrap">
+                        <img src="img/img2.png" class="news_list_img">
+                        <div class="news_list_more_mask">MORE</div>
+                    </div>
+                    <div class="news_list_content">
+                        <div class="news_list_date">2022/05/01</div>
+                        <div class="news_list_title">最新消息標題最新消息標題</div>
+                        <div class="news_list_summary">最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要</div>
+                    </div>
+                </a>
+            </div>
+            <div class="news_list">
+                <a href="#" class="news_list_link">
+                    <div class="news_list_img_wrap">
+                        <img src="img/img2.png" class="news_list_img">
+                        <div class="news_list_more_mask">MORE</div>
+                    </div>
+                    <div class="news_list_content">
+                        <div class="news_list_date">2022/05/01</div>
+                        <div class="news_list_title">最新消息標題最新消息標題</div>
+                        <div class="news_list_summary">最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要</div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="news_list">
+                <a href="#" class="news_list_link">
+                    <div class="news_list_img_wrap">
+                        <img src="img/img2.png" class="news_list_img">
+                        <div class="news_list_more_mask">MORE</div>
+                    </div>
+                    <div class="news_list_content">
+                        <div class="news_list_date">2022/05/01</div>
+                        <div class="news_list_title">最新消息標題最新消息標題</div>
+                        <div class="news_list_summary">最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要</div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="news_list">
+                <a href="#" class="news_list_link">
+                    <div class="news_list_img_wrap">
+                        <img src="img/img2.png" class="news_list_img">
+                        <div class="news_list_more_mask">MORE</div>
+                    </div>
+                    <div class="news_list_content">
+                        <div class="news_list_date">2022/05/01</div>
+                        <div class="news_list_title">最新消息標題最新消息標題</div>
+                        <div class="news_list_summary">最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要</div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="news_list">
+                <a href="#" class="news_list_link">
+                    <div class="news_list_img_wrap">
+                        <img src="img/img2.png" class="news_list_img">
+                        <div class="news_list_more_mask">MORE</div>
+                    </div>
+                    <div class="news_list_content">
+                        <div class="news_list_date">2022/05/01</div>
+                        <div class="news_list_title">最新消息標題最新消息標題</div>
+                        <div class="news_list_summary">最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要</div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="news_list">
+                <a href="#" class="news_list_link">
+                    <div class="news_list_img_wrap">
+                        <img src="img/img2.png" class="news_list_img">
+                        <div class="news_list_more_mask">MORE</div>
+                    </div>
+                    <div class="news_list_content">
+                        <div class="news_list_date">2022/05/01</div>
+                        <div class="news_list_title">最新消息標題最新消息標題</div>
+                        <div class="news_list_summary">最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要</div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="news_list">
+                <a href="#" class="news_list_link">
+                    <div class="news_list_img_wrap">
+                        <img src="img/img2.png" class="news_list_img">
+                        <div class="news_list_more_mask">MORE</div>
+                    </div>
+                    <div class="news_list_content">
+                        <div class="news_list_date">2022/05/01</div>
+                        <div class="news_list_title">最新消息標題最新消息標題</div>
+                        <div class="news_list_summary">最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要最新消息摘要</div>
+                    </div>
+                </a>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<?php include('php/foot.php'); ?>
